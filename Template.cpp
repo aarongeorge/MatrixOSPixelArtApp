6a9e2e3d9cebf0f72f60bd99e212c79254d829b5
Nengzhuo Cai <null@203.io>
2025-09-15 20:55:13 -0400

    Initial release

---
+++ Template.cpp
@@ -0,0 +1,16 @@
+#include "Template.h"
+
+// Run once
+void Template::Setup() {
+  // Write Logics that happens when app first starts
+}
+
+// Run in a loop after Setup()
+void Template::Loop() {
+  // Write logics that should keep happens
+
+}
+
+void Template::End() {
+  // Write logic that should happen when app exits
+}
---
+++ Template.h
@@ -0,0 +1,23 @@
+/*
+This is the template application for Matrix OS
+*/
+
+#pragma once
+
+#include "MatrixOS.h"
+#include "Application.h"
+
+class Template : public Application {
+ public:
+  inline static Application_Info info = {
+      .name = "Template",
+      .author = "203 Systems",
+      .color = Color(0xFFFFFF),
+      .version = 1,
+      .visibility = true,
+  };
+
+  void Setup() override;
+  void Loop() override;
+  void End() override;
+};
